52eade4cc2bc2e1a0cec5569060adb9354f33a93
Aleksander Prikhozhdenko <aprikhozhdenko@plesk.com>
2022-03-31 20:37:10 +0700

    Rearrange environment variables

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -8,16 +8,17 @@ if $(wp core is-installed); then
     exit;
 fi
 
-wp core install --skip-email --title='WSP AWS' --admin_user=WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
-    --admin_email=$WORDPRESS_ADMIN_EMAIL  --url=$WORDPRESS_URL
+wp core install --skip-email --title='WSP AWS' --url=$WORDPRESS_URL \
+    --admin_user=WORDPRESS_ADMIN_USER --admin_email=$WORDPRESS_ADMIN_EMAIL --admin_password=$WORDPRESS_ADMIN_PASSWORD
+
 wp theme install storefront --activate \
     && wp plugin install woocommerce --activate \
-    && wp --user=1 wc product create --name="Example of a simple product" --type="simple" --regular_price="1.00" \
+    && wp --user=1 wc product create --name="Example of a simple product" --type="simple" --regular_price="11.00" \
     && wp --user=1 wc product create --name="Example of an variable product" --type="variable" \
        --attributes='[ { "name":"size", "variation":"true", "options":"X|XL" } ]' \
     && wp --user=1 wc product_variation create 11 \
-       --attributes='[ { "name":"size", "option":"X" } ]' --regular_price="1.00" \
+       --attributes='[ { "name":"size", "option":"X" } ]' --regular_price="51.00" \
     && wp --user=1 wc product_variation create 11 \
-       --attributes='[ { "name":"size", "option":"XL" } ]' --regular_price="2.00"
+       --attributes='[ { "name":"size", "option":"XL" } ]' --regular_price="52.00"
 
 echo >&2 "Complete! WordPress has been configured"
